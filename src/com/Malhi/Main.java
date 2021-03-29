b30e1863bab65bc2eaa46f9f6f869518bc7e1dcb
LabhiMalhi <lovepreetwm98@gmail.com>
2021-03-29 00:18:25 -0400

    Sums

---
+++ src/com/Malhi/Main.java
@@ -0,0 +1,18 @@
+package com.Malhi;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[][] a = {{1,4,5,6},{3,7,2,6},{23,42,4,34},{21,35,56,63}};
+        int sum1 = 0,sum2,sum3,sum4;
+        for(int i = 0;i < a.length;i++){
+            for(int j = 0; j < a[i].length; i++){
+                System.out.println(a[i][j]);
+            }
+
+        }
+    }
+}
